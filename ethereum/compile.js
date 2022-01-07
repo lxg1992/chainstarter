41097cac2d5114681df7a29a11199047f5844c49
Alex <lxg1992@gmail.com>
2022-01-07 08:40:37 -0500

    Added tests and contract

---
+++ ethereum/compile.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra');
+
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
+
+const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+const source = fs.readFileSync(campaignPath, 'utf8');
+const output = solc.compile(source, 1).contracts;
+
+//Ensure path exists, create if not.
+fs.ensureDirSync(buildPath);
+
+for (let contract in output) {
+  fs.outputJSONSync(
+    path.resolve(buildPath, contract.replace(':', '') + '.json'),
+    output[contract]
+  )
+}
---
+++ test/Campaign.test.js
@@ -0,0 +1,39 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const buildPath = '../ethereum/build/';
+
+const compiledFactory = require(buildPath + '/CampaignFactory.json');
+const compiledCampaign = require(buildPath + '/Campaign.json');
+
+let accounts;
+let factory;
+let campaignAddress;
+let campaign;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+
+  factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({
+      data: compiledFactory.bytecode 
+    })
+    .send({
+      from: accounts[0], gas: '1000000'
+    });
+
+  await factory.methods.createCampaign('100').send({
+    from: accounts[0],
+    gas: '1000000'
+  });
+
+  [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+
+  campaign = await new web3.eth.contract(
+    JSON.parse(compiledCampaign.interface),
+    campaignAddress
+  )
+
+})
